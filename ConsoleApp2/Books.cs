3c6f54aeea981b530880eed6784e90d1d54f5ce8
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-01-28 21:28:00 +0200

    Add files via upload

---
+++ ConsoleApp2/Books.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Books
+    {
+        /// <summary>
+        /// название книги
+        /// </summary>
+        public string name="World and piece";
+        /// <summary>
+        /// автор книги
+        /// </summary>       
+        public string avtor="Tolstoy";
+        /// <summary>
+        /// год выпуска
+        /// </summary>
+        public int year=1863;
+        /// <summary>
+        /// объем в листах
+        /// </summary>
+        public int count=1696;
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Название книги: {name}\nАвтор книги: {avtor}\nГод выпуска: {year}\nОбъем в листах:{count}");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Books books = new Books();
+            books.Print();
+            Console.WriteLine();
+            //2
+            string nameBooks = books.name;
+            string avtorBooks = books.avtor;
+            int yearBooks=books.year;
+            int countBooks=books.count;
+            Console.WriteLine($"Название книги: {nameBooks}\nАвтор книги: {avtorBooks}\nГод выпуска: {yearBooks}\nОбъем в листах:{countBooks}");
+            Console.WriteLine();
+            //3
+            books.name = "Преступление и наказание";
+            books.avtor = "Dostoevskii";
+            books.year = 2023;
+            books.count = 400;
+            books.Print();
+            Console.WriteLine();
+            //4
+            Console.Write("Название книги : ");
+            books.name =Console.ReadLine();
+            Console.Write("Автор книги : ");
+            books.avtor = Console.ReadLine();
+            Console.Write("Год выпуска : ");
+                books.year=int.Parse(Console.ReadLine());
+            Console.Write("Объем в листах : ");
+                books.count = int.Parse(Console.ReadLine());
+            books.Print();
+            Console.Read();
+        }
+    }
+}
